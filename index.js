6181477ca339beb79610ed2a8ff03bd4c4a5eeab
Jony <jony378892@gmail.com>
2025-11-13 15:42:10 +0600

    Added initial path for request in backend

--- index.js
+++ index.js
@@ -3,39 +3,13 @@ import fs from "fs";
 import cors from "cors";
 import dotenv from "dotenv";
 import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
-import admin from "firebase-admin";
 
 dotenv.config();
-
-const serviceAccount = JSON.parse(
-  fs.readFileSync("./firebase-admin-sdk.json", "utf-8")
-);
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-const verifyToken = async (req, res, next) => {
-  const authorization = req.headers.authorization;
-  const token = authorization.split(" ")[1];
-
-  if (!token || !authorization) {
-    return res.status(403).send({ message: "Unauthorized access" });
-  }
-
-  try {
-    const decode = await admin.auth().verifyIdToken(token);
-    req.token_email = decode.email;
-    next();
-  } catch (error) {
-    res.status(403).send({ message: "Forbidden" });
-  }
-};
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const uri = process.env.MONGO_URI;
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.aopfsxd.mongodb.net/?appName=Cluster0`;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -47,14 +21,18 @@ const client = new MongoClient(uri, {
 app.use(cors());
 app.use(express.json());
 
+app.use("/", (req, res) => {
+  res.send("Server is ok");
+});
+
 async function run() {
   try {
     await client.connect();
 
     const db = client.db("homenest");
     const usersCollection = db.collection("users");
-    const propertiesCollection = db.collection("properties");
     const ratingsCollection = db.collection("ratings");
+    const propertiesCollection = db.collection("properties");
     const citiesCollection = db.collection("cities");
 
     app.post("/users", async (req, res) => {
@@ -78,62 +56,93 @@ async function run() {
     });
 
     app.get("/properties", async (req, res) => {
-      const cursor = propertiesCollection.find({});
+      const email = req.query.email;
+      // console.log(email);
+      const query = {};
+      if (email) {
+        query.userEmail = email;
+      }
+
+      const cursor = propertiesCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     });
 
-    app.get("/latest-properties", async (req, res) => {
+    app.get("/featured", async (req, res) => {
       const cursor = propertiesCollection.find();
       const result = await cursor.sort({ inserted_at: -1 }).limit(6).toArray();
       res.send(result);
     });
 
-    app.post("/add-property", verifyToken, async (req, res) => {
-      const property = req.body;
-      try {
-        if (req.token_email === property.email) {
-          const result = await propertiesCollection.insertOne(property);
-          return res.send(result);
-        } else {
-          return res.send({ message: "Unauthorized access" });
-        }
-      } catch (error) {
-        console.log(error.message);
-      }
-    });
-
     app.get("/property/:id", async (req, res) => {
       const id = req.params.id;
+      // console.log(id);
       const query = { _id: new ObjectId(id) };
-      const result = await propertiesCollection.findOne({ query });
+      const result = await propertiesCollection.findOne(query);
       res.send(result);
     });
 
-    app.post("/property/:id", verifyToken, async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const update = {
-        $set: req.body,
-      };
-      const options = {};
-      const result = await propertiesCollection.updateOne(
-        query,
-        update,
-        options
-      );
-      res.send(result);
+    app.post("/add-property", async (req, res) => {
+      const property = req.body;
+      const result = await propertiesCollection.insertOne(property);
+      return res.send(result);
     });
 
-    app.post("/delete-property/:id", verifyToken, async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const { email } = req.body;
-      const existProperty = await propertiesCollection.findOne(query);
+    app.patch("/update-property/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
 
-      if (req.token_email !== email) {
-        return res.send({ message: "Unauthorized access" });
+        const {
+          propertyName,
+          shortDescription,
+          category,
+          propertyPrice,
+          location,
+          image,
+        } = req.body;
+
+        const updateFields = {};
+        if (propertyName) updateFields.propertyName = propertyName;
+        if (shortDescription) updateFields.shortDescription = shortDescription;
+        if (category) updateFields.category = category;
+        if (propertyPrice) updateFields.propertyPrice = Number(propertyPrice);
+        if (location) updateFields.location = location;
+        if (image) updateFields.image = image;
+
+        updateFields.updatedAt = new Date();
+
+        const updateDoc = { $set: updateFields };
+
+        const result = await propertiesCollection.updateOne(query, updateDoc);
+
+        if (result.modifiedCount > 0) {
+          const updatedProperty = await propertiesCollection.findOne(query);
+          return res.send({
+            success: true,
+            message: "Property updated successfully",
+            updatedProperty,
+          });
+        } else {
+          return res.status(404).send({
+            success: false,
+            message: "Property not found or no changes detected",
+          });
+        }
+      } catch (err) {
+        console.error(err);
+        res.status(500).send({
+          success: false,
+          message: "Failed to update property",
+        });
       }
+    });
+
+    app.delete("/delete-property/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const query = { _id: new ObjectId(id) };
+      const existProperty = await propertiesCollection.findOne(query);
 
       if (!existProperty) {
         res.send({ message: "The property not found" });
@@ -143,24 +152,16 @@ async function run() {
       }
     });
 
-    app.get("/ratings", verifyToken, async (req, res) => {
+    app.get("/ratings", async (req, res) => {
       const email = req.query.email;
-      console.log(email);
       const query = { email };
       const cursor = ratingsCollection.find(query);
-      if (req.token_email !== email) {
-        return res.status(403).send({ message: "Forbidden" });
-      }
 
       const result = await cursor.toArray();
       res.send(result);
     });
 
-    app.post("/ratings", verifyToken, async (req, res) => {
-      if (req.token_email !== email) {
-        return res.status(403).send({ message: "Forbidden" });
-      }
-
+    app.post("/ratings", async (req, res) => {
       const data = req.body;
       const result = await ratingsCollection.insertOne(data);
       res.send(result);
